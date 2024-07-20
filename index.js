6a18d6e6bfdcaeccbc12ce945fb73b5a8c8d9926
Jul-Techlore <ofojuliet3@gmail.com>
2024-07-20 16:57:42 -0700

    first-javascript

---
+++ index.js
@@ -0,0 +1,173 @@
+// console.log("Welcome")
+// console.error("Welcome")
+// console.warn("Welcome")
+// let Emekaage=56
+// Emekaage=21;
+// Emekaage=36;
+// console.log(Emekaage);
+// const Emekaage=67;
+// console.log(Emekaage);
+// let score;
+// score="Welcome";
+// score="Tony"
+// console.log(score);
+
+// data types starts here
+// const firstName="Peter";
+// const lastName="Obi";
+// const age=63;
+// const isAdmin=false;
+// const x =null;
+// const y=undefined;
+// console.log("My name is" + firstName + lastName + "I am" + age + "years old.");
+
+// console.log(`My name is ${firstName} ${lastName} . I am ${age}  years old. I am an ${isAdmin}`);
+// console.log(`My name is ${firstName} ${lastName} . I am ${age} years old . I am ${isAdmin} . The last election was ${x} . And the result was ${y} .`);
+
+// To check the number of the alphabets , including the space starts here
+// const word = "Hello World";
+// console.log(word.split(""));
+// console.log(word.length);
+
+// const tools = "Knife, book, pencil, crayon";
+// console.log(tools.split());
+
+// const tools = "Knife, book, pencil, crayon";
+// console.log(tools);
+
+// const tools = "Knife, book, pencil, crayon";
+// console.log(tools.split());
+
+// To split each alphabet starts here
+// const tools = "Knife, book, pencil, crayon";
+// console.log(tools.split(""));
+
+// To split each word starts here
+// const tools = "Knife, book, pencil, crayon";
+// console.log(tools.split(","));
+
+// string data type starts here
+// const firstName = "Peter";
+// const lastName = "Obi";
+// const age = 63;
+// const isAdmin = false;
+// const x = null;
+// const y = undefined;
+// console.log(typeof firstName);
+
+// number data type starts here
+// const firstName = "Peter";
+// const lastName = "Obi";
+// const age = 63;
+// const isAdmin = false;
+// const x = null;
+// const y = undefined;
+// console.log(typeof age);
+
+// boolean data type starts here
+// const firstName = "Peter";
+// const lastName = "Obi";
+// const age = 63;
+// const isAdmin = false;
+// const x = null;
+// const y = undefined;
+// console.log(typeof isAdmin);
+
+// object data type starts here
+// const firstName = "Peter";
+// const lastName = "Obi";
+// const age = 63;
+// const isAdmin = false;
+// const x = null;
+// const y = undefined;
+// console.log(typeof x);
+
+// undefined data type starts here
+// const firstName = "Peter";
+// const lastName = "Obi";
+// const age = 63;
+// const isAdmin = false;
+// const x = null;
+// const y = undefined;
+// console.log(typeof y);
+
+// Array starts here
+// const listItems = ["Beans", "Rice", "Garri", 40, true, null, undefined];
+// console.log(listItems);
+
+// To target Rice starts here
+// const listItems = ["Beans", "Rice", "Garri", 40, true, null, undefined];
+// console.log(listItems[1]);
+
+// To display 40 starts here
+// const listItems = ["Beans", "Rice", "Garri", 40, true, null, undefined];
+// console.log(listItems[3]);
+
+// to check the length
+// const listItems = ["Beans", "Rice", "Garri", 40, true, null, undefined];
+// console.log(listItems);
+
+// To add something to the Array at the end of the array, use the .push method starts here
+// const listItems = ["Beans", "Rice", "Garri", 40, true, null, undefined];
+// listItems.push("Goat", 58, "Computer");
+// console.log(listItems);
+// console.log(listItems);
+
+// To remove the last value from the array , use .pop method starts here
+// const listItems = ["Beans", "Rice", "Garri", 40, true, null, undefined];
+// listItems.push("Goat", 58, "Computer");
+// listItems.pop();
+// console.log(listItems);
+
+// To add a new value to the array and you want that value to be the first value inside the array, use .unshift starts here
+// const listItems = ["Beans", "Rice", "Garri", 40, true, null, undefined];
+// listItems.unshift("Pen", "Chalk");
+// console.log(listItems);
+
+// To represent array data[] inside an object{} , starts here
+// const studentInfo = {
+//   firstName: "Favour",
+//   lastName: "Amaka",
+//   age: 13,
+//   hobbies: ["dancing", "sports", "twerking", 89],
+//   hasPaidSchoolFees: true,
+//   x: null,
+//   isWicked: undefined,
+// };
+// console.log(studentInfo);
+
+// To display the age starts here
+// const studentInfo = {
+//   firstName: "Favour",
+//   lastName: "Amaka",
+//   age: 13,
+//   hobbies: ["dancing", "sports", "twerking", 89],
+//   hasPaidSchoolFees: true,
+//   x: null,
+//   isWicked: undefined,
+// };
+// console.log(studentInfo.age);
+
+// To consolog first name starts here
+// const studentInfo = {
+//   firstName: "Favour",
+//   lastName: "Amaka",
+//   age: 13,
+//   hobbies: ["dancing", "sports", "twerking", 89],
+//   hasPaidSchoolFees: true,
+//   x: null,
+//   isWicked: undefined,
+// };
+// console.log(studentInfo.age);
+
+// To display twerking starts here
+const studentInfo = {
+  firstName: "Favour",
+  lastName: "Amaka",
+  age: 13,
+  hobbies: ["dancing", "sports", "twerking", 89],
+  hasPaidSchoolFees: true,
+  x: null,
+  isWicked: undefined,
+};
+console.log(studentInfo.hobbies[2]);
